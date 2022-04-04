ba3fed1049f343e97dd4e74fb7bd9f3f94b17497
slauko <slauko94@gmail.com>
2022-04-04 15:05:26 +0200

    added routes

--- server/server.js
+++ server/server.js
@@ -5,9 +5,41 @@ const mongoose = require('mongoose');
 const config = require('./config/config');
 const port = process.env.PORT || 3000;
 
+// MongoDB connection
+mongoose.connect(config.database, { useNewUrlParser: true });
+
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+// GET pokemon list
+app.get('/pokemon', (req, res) => {
+	const pokemon = require('./data/pokemon.json');
+	res.send(pokemon);
+});
+
+// GET pokemon by id
+app.get('/pokemon/:id', (req, res) => {
+	const pokemon = require('./data/pokemon.json');
+	const id = req.params.id;
+	const pokemonById = pokemon.find((pokemon) => pokemon.id === id);
+	res.send(pokemonById);
+});
+
+// GET pokemon by id and info
+app.get('/pokemon/:id/:info', (req, res) => {
+	const pokemon = require('./data/pokemon.json');
+	const id = req.params.id;
+	const info = req.params.info;
+	const pokemonById = pokemon.find((pokemon) => pokemon.id === id);
+	const pokemonInfo = pokemonById[info];
+	res.send(pokemonInfo);
+});
+
+// Handle 404
+app.use((req, res) => {
+	res.status(404).send('404: Page not found');
+});
 
 // Listen to port
 app.listen(port, () => {
