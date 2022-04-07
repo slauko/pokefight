af35dcc38c9586fac0680e921f4d6eddf7e8972d
slauko <slauko@users.noreply.github.com>
2022-04-07 16:29:30 +0200

    backcards, footer styling

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../styles/Footer.css';
 import { FaGithub } from 'react-icons/fa';
+import { IconContext } from 'react-icons/lib';
 export default function Footer() {
 	return (
 		<div className='Footer row'>
@@ -10,10 +11,21 @@ export default function Footer() {
 					<p>&copy; 2022 WBS-WDG003 Group 4</p>
 				</div>
 				<div className='Socials col-md'>
-					<FaGithub size={25} />
-					<FaGithub size={25} />
-					<FaGithub size={25} />
-					<FaGithub size={25} />
+					<IconContext.Provider
+						value={{
+							size: '30px',
+							style: {
+								borderRadius: '50%',
+								boxShadow:
+									'1px 1px 2px black, -1px -1px 2px black, 1px -1px 2px black, -1px 1px 2px black, inset 0px 0px 15px black',
+							},
+						}}
+					>
+						<FaGithub />
+						<FaGithub />
+						<FaGithub />
+						<FaGithub />
+					</IconContext.Provider>
 				</div>
 			</div>
 		</div>
